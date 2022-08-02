7989828f6fb81bd431feadd8ef90d59bd5eeef58
Thomas <thomas.lawanson@gmail.com>
2022-08-02 09:33:59 +0100

    Create General layout
    This layout has the header, footer and a slotted components

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+</script>
+
+<main>
+	<Header />
+    <slot />
+    <footer> Comparts show </footer>
+	
+</main>
+
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+</style>
