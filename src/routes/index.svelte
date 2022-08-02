c328c6cc89f63c33ddf7bbd5a3351fafe2f69ca3
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-08-02 12:01:13 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	export const prerender = true;
 	import { base, assets } from '$app/paths';
 	import Seo from '$lib/seo/Seo.svelte';
+	import EllipseSketch from '$lib/sketches/EllipseSketch.svelte';
 
 	import '../app.css';
 	import P5 from 'p5-svelte';
@@ -24,24 +25,7 @@
 
 <Seo title="Home Page" />
 
-<main>
-	<section>
-
-		<Counter />
-		<P5 {sketch} />
-		<label>
-			Width
-			<input type="range" bind:value={width} min="100" max="1000" step="0.01" />
-			{width}
-		</label>
-		
-		<label>
-			Height
-			<input type="range" bind:value={height} min="100" max="1000" step="0.01" />
-			{height}
-		</label>
-	</section>
-</main>
+<EllipseSketch />
 
 <style>
 	main {
