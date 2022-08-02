ab01b6f19862c54a5a5e784c81dc1e8ee68ed667
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2022-08-02 12:46:19 +0100

    Update artworks.svelte

--- src/routes/artworks.svelte
+++ src/routes/artworks.svelte
@@ -16,15 +16,44 @@
 	// since there's no dynamic data here, we can prerender
 	// it so that it gets served as a static asset in prod
 	export const prerender = true;
+
+	let artists = Array(9).fill(0);
 </script>
 
 
 <Seo title="Art" description={"this is all the artwork and artists"} />
 
-<div>
+<section>
 	<p> Page about art</p>
-</div>
+	<div class="div__artists-grid">
+
+
+		{#each artists as artist}
+			<div class="div--image-tile">
+				<img class="img__responsive" src="/img/1.png" alt="img" />
+				<h3 class="p--art-title"> Title of Art</h3>
+				<p> Artist's Name</p>
+			</div>
+
+		{/each}
+
+	</div>	
+
+</section>
 
 
 <style>
+	.div__artists-grid {
+		display: grid;
+		width: 100%;
+		grid-template-columns: repeat(3, 1fr);
+	}
+
+	.div--image-tile {
+		padding: 0.5rem;
+	}
+
+	.p--art-title {
+
+	}
 </style>
