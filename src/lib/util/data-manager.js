cdd9269f5d129d7e812b227031703ca7079b89cd
Thomas <thomas.lawanson@gmail.com>
2022-08-05 01:50:47 +0100

    Added comments

--- src/lib/util/data-manager.js
+++ src/lib/util/data-manager.js
@@ -4,6 +4,13 @@ import { shuffle } from 'lodash';
 import { slugify } from './helper';
 
 export class DataManager {
+    /**
+     * Get shuffled Artwork List
+     *
+     * @static
+     * @return {*} 
+     * @memberof DataManager
+     */
     static getArtworkList() {
         let artworks = ArtworksCSV;
         artworks = artworks.map((artwork) => {
@@ -12,18 +19,51 @@ export class DataManager {
         return shuffle(artworks); // randomised
     }
 
-    static getArtwork(preferredName) {
-        let artworks = ArtworksCSV;
-        return shuffle(artworks);
+    /**
+     * Get Artwork based on slug
+     *
+     * @static
+     * @param {*} slug
+     * @return {*} 
+     * @memberof DataManager
+     */
+    static getArtwork(slug) {
+        let artworks = DataManager.getArtworkList();
+
+        return  artworks.find((artwork) => {
+            return artwork.slug === slug.toLowerCase()
+        })
     }
 
+    /**
+     * Get shuffled Artist list
+     *
+     * @static
+     * @return {*} 
+     * @memberof DataManager
+     */
     static getArtistList() {
         let artists =  ArtistCSV;
         artists = artists.map((artist) => {
             return slugify(artist);
         })
         return shuffle(artists); // randomised
+    }
 
+    /**
+     * Get artist based on Slug
+     *
+     * @static
+     * @param {*} slug
+     * @return {*} 
+     * @memberof DataManager
+     */
+    static getArtist(slug) {
+        let artists = DataManager.getArtistList();
+
+        return  artists.find((artist) => {
+            return artist.slug === slug.toLowerCase()
+        })
     }
 
 }
